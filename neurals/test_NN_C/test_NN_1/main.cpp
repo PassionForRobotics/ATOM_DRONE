5dea1d6e8207174665bd6e9cb8cd7cddfb5c310c
rahuldeo2047 <rahuldeo2047@gmail.com>
2017-04-19 06:35:51 +0530

    Issue #8 Neural nets csv format correction attempted to match github csv format

--- neurals/test_NN_C/test_NN_1/main.cpp
+++ neurals/test_NN_C/test_NN_1/main.cpp
@@ -394,7 +394,7 @@ void writeToaFile()
     for ( j = 0 ; j < HiddenNodes+1 ; j++ ) {
       fprintf(fp, "%f,", OutputWeights[j][i]);
     }
-    fprintf(fp, "\r\n");
+    fprintf(fp, ",\r\n");
   }
   fprintf(fp, "End");
   for (i = 0; i < HiddenNodes+1; i++) {
