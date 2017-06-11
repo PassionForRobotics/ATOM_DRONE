895d57afa6679bf933b79056cc9099d38bfbe294
rahuldeo2047 <rahuldeo2047@gmail.com>
2017-06-11 23:13:40 +0530

    Issue #24 [ESP8266_D1_R1] All four servos tested, to fine tune

--- ESP8266_Custom/ESP8266_Custom/version.h
+++ ESP8266_Custom/ESP8266_Custom/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define _VER_ ( "v0.0.1-15-g9d631a2" )
+#define _VER_ ( "v0.0.1-16-gb6d0065" )
 
 #endif //VERSION_H
--- ESP8266_D1/src/steer.ino
+++ ESP8266_D1/src/steer.ino
@@ -8,6 +8,7 @@ int i = 0;
 
 Servo ESC; // D1 pin D12
 
+#define Servo1_offset 1;
 
 void steer_setup()
 {
@@ -30,7 +31,13 @@ void steer_loop(sMPUDATA_t *mpudata, sMOTIONSETPOINTS_t *msetpts)
 {
   byte a = random(90-45, 90+45);
 
-  byte x = map(msetpts->x, 0, 512, -45, 45);
+  byte x = map(msetpts->x, 0, 1023, -45+90, 45+90);
+  byte x1 = map(msetpts->x, 1023, 0, -45+90, 45+90);
+  //x += 90;
+  byte y = map(msetpts->y, 0, 1023, -45+90, 45+90);
+  byte y1 = map(msetpts->y, 1023, 0, -45+90, 45+90);
+
+  //y += 90;
 
   if(system_get_time()-lastSteerLoopTime >=(STEER_LOOP_TIME))
   {
@@ -39,7 +46,12 @@ void steer_loop(sMPUDATA_t *mpudata, sMOTIONSETPOINTS_t *msetpts)
     //for(i=0;i<4;i++)
     {
       //Serial.printf("rnd %d\n", angle);
-      servo[0].write(x);//random(90-45, 90+45));
+      servo[0].write(x);//random(90-45, 90+45));byte x = map(msetpts->x, 0, 512, -45, 45);
+
+      servo[1].write(y);
+
+      servo[2].write(x1);
+      servo[3].write(y1);
     }
     //ESC.write(angle);
   }
