8255f94a6a103c54a5105fdb261e49a5c9d31d08
rahuldeo2047 <rahuldeo2047@gmail.com>
2017-08-14 15:27:32 +0530

    Issue #3 [mech] servo alignment

--- ESP8266_D1/src/steer.ino
+++ ESP8266_D1/src/steer.ino
@@ -8,9 +8,9 @@ int i = 0;
 
 // Servo mechanical offsets
 const byte SERVO0_OFFSET = (7);
-const byte SERVO1_OFFSET = (4);
-const byte SERVO2_OFFSET = (6);
-const byte SERVO3_OFFSET = (-5);
+const byte SERVO1_OFFSET = (-1) ;//(4);
+const byte SERVO2_OFFSET = (13);
+const byte SERVO3_OFFSET = (-10) ;//(-5);
 
 #define MAX_THROTTLE (2000) //(1864)
 #define MIN_THROTTLE (1064)
@@ -35,8 +35,24 @@ void steer_setup()
   for(i=0;i<4;i++)
   {
     servo[i].attach(pins[i]);  // attaches the servo on GIO2 to the servo object
-    servo[i].write(90+servo_offsets[i]);
+    //servo[i].write(90+servo_offsets[i]);
   }
+
+  byte x = map(512, 1023, 0, -45+90+servo_offsets[0], 45+90+servo_offsets[0]);
+  //x += 90;
+  byte y = map(512, 0, 1023, -45+90+servo_offsets[1], 45+90+servo_offsets[1]);
+
+  byte x1 = map(512, 0, 1023, -45+90+servo_offsets[2], 45+90+servo_offsets[2]);
+
+  byte y1 = map(512, 1023, 0, -45+90+servo_offsets[3], 45+90+servo_offsets[3]);
+
+  servo[0].write(x);
+
+  servo[1].write(y);
+
+  servo[2].write(x1);
+
+  servo[3].write(y1);
 }
 
 byte angle = 35;
@@ -48,11 +64,21 @@ void steer_loop(sMPUDATA_t *mpudata, sMOTIONSETPOINTS_t *msetpts)
 
   //byte a = random(90-45, 90+45);
 
-  byte x = map(msetpts->x, 0, 1023, -45+90+servo_offsets[0], 45+90+servo_offsets[0]);
+  // byte x = map(msetpts->x, 0, 1023, -45+90+servo_offsets[0], 45+90+servo_offsets[0]);
+  // //x += 90;
+  // byte y = map(msetpts->y, 0, 1023, -45+90+servo_offsets[1], 45+90+servo_offsets[1]);
+  //
+  // byte x1 = map(msetpts->x, 1023, 0, -45+90+servo_offsets[2], 45+90+servo_offsets[2]);
+  //
+  // byte y1 = map(msetpts->y, 1023, 0, -45+90+servo_offsets[3], 45+90+servo_offsets[3]);
+  //
+  // byte tw = map(msetpts->twist, 0, 255, -SERVO_TWIST_ANGLE_LIMIT, SERVO_TWIST_ANGLE_LIMIT);
+
+  byte x = map(msetpts->x, 1023, 0, -45+90+servo_offsets[0], 45+90+servo_offsets[0]);
   //x += 90;
   byte y = map(msetpts->y, 0, 1023, -45+90+servo_offsets[1], 45+90+servo_offsets[1]);
 
-  byte x1 = map(msetpts->x, 1023, 0, -45+90+servo_offsets[2], 45+90+servo_offsets[2]);
+  byte x1 = map(msetpts->x, 0, 1023, -45+90+servo_offsets[2], 45+90+servo_offsets[2]);
 
   byte y1 = map(msetpts->y, 1023, 0, -45+90+servo_offsets[3], 45+90+servo_offsets[3]);
 
@@ -81,7 +107,7 @@ void steer_loop(sMPUDATA_t *mpudata, sMOTIONSETPOINTS_t *msetpts)
     //for(i=0;i<4;i++)
     {
       //Serial.printf("rnd %d\n", angle);
-      servo[0].write(x);//random(90-45, 90+45));byte x = map(msetpts->x, 0, 512, -45, 45);
+      servo[0].write(x);
 
       servo[1].write(y);
 
--- ESP8266_D1/src/version.h
+++ ESP8266_D1/src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define _VER_ ( "v0.0.2-1-gdb06c6c" )
+#define _VER_ ( "v0.0.3-0-gd2e0a32" )
 
 #endif //VERSION_H
